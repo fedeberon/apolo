199f45c760cfe35c92dce3809074489b4f76f632
damiansaez <damiansaez12@hotmail.com>
2018-03-07 09:50:42 -0300

    Creacion de controller e interface Usuario

---
+++ src/main/java/com/bolivarSoftware/apolo/web/usuario/UsuarioController.java
@@ -0,0 +1,7 @@
+package com.bolivarSoftware.apolo.web.usuario;
+
+/**
+ * Created by Damian on 07/03/2018.
+ */
+public class UsuarioController {
+}
---
+++ src/main/java/com/bolivarSoftware/apolo/web/usuario/interfaces/IUsuario.java
@@ -0,0 +1,7 @@
+package com.bolivarSoftware.apolo.web.usuario.interfaces;
+
+/**
+ * Created by Damian on 07/03/2018.
+ */
+public interface IUsuario {
+}

8f1fef6e2fd73ca3e0ada4c14db4124ccf39fcdf
damiansaez <damiansaez12@hotmail.com>
2018-03-07 10:45:28 -0300

    Delete apolo.iml

---
+++ src/main/java/com/bolivarSoftware/apolo/services/interfaces/IUsuarioService.java
@@ -0,0 +1,7 @@
+package com.bolivarSoftware.apolo.services.interfaces;
+
+/**
+ * Created by Damian on 07/03/2018.
+ */
+public interface IUsuarioService {
+}
---
+++ src/main/java/com/bolivarSoftware/apolo/services/usuario/UsuarioService.java
@@ -0,0 +1,7 @@
+package com.bolivarSoftware.apolo.services.usuario;
+
+/**
+ * Created by Damian on 07/03/2018.
+ */
+public class UsuarioService {
+}
